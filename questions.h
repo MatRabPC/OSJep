60529b96863bcc4493914dd1f3908baa27547701
rsamidev <samina.rahmanyar@uoit.net>
2017-02-21 19:38:40 -0500

    Caps Locked

--- questions.h
+++ questions.h
@@ -5,49 +5,206 @@
  * All rights reserved.
  *
  */
-#ifndef QUESTIONS_H_
-#define QUESTIONS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "questions.h"
 
-#include <stdbool.h>
+question quest[NUM_QUESTIONS] = {
 
-#define MAX_LEN 256
-#define NUM_CATEGORIES 3
-// The number of questions, you can use this in your functions in
-// questions.c, this can be accessed in questions.c
-#define NUM_QUESTIONS 12
+{
+    "BEATLES",
+    "This one was married to Yoko Ono",
+    "JOHN",
+    100,
+    false
+},
 
-// List of 3 categories as array of strings
-static char categories[NUM_CATEGORIES][MAX_LEN] = {
-    "BEATLES", 
-    "INSECTS", 
-    "BUGS"
+{
+    "BEATLES",
+    "This one played bass",
+    "PAUL",
+    200,
+    false
+},
+
+{
+    "BEATLES",
+    "This one died in 1966",
+    "PAUL", 
+    300,
+    false
+},
+
+{
+    "BEATLES",
+    "This one lifts 850 times its body mass",
+    "HERCULES",
+    400,
+    false
+},
+
+{
+    "INSECTS",
+    "This one spins a web",
+    "SPIDER",
+    100,
+    false
+},
+{
+    "INSECTS",
+    "This one is named after its mode of transportation",
+    "FLY",
+    200,
+    false
+},
+
+{
+    "INSECTS",
+    "This one crawls on the ground",
+    "WORM",
+     300,
+    false
+},
+
+{
+    "INSECTS",
+    "This one is not margerine and doesn't walk",
+    "BUTTERFLY",
+    400,
+    false
+},
+
+{
+    "BUGS",
+    "Number when page not found",
+    "404",
+    100,
+    false
+},
+
+{
+    "BUGS",
+    "I couldn't think of another one",
+"404",
+    200,
+    false
+},
+
+{
+    "BUGS",
+     "I couldn't think of one more'",
+  "404",
+    300,
+    false
+},
+
+{
+    "BUGS",
+    "Name in Hyrule",
+    "ERROR",
+    400,
+    false
+}
 };
 
-// Questions struct for each question
-typedef struct {
-    char category[MAX_LEN];
-    char question[MAX_LEN];
-    char answer[MAX_LEN];
-    int value;
-    bool answered;
-} question;
-
-// An array of 12 questions (4 for each category), initialized in initialize_game
-// this may need to be a pointer if you want it set dynamically
-
 // Initializes the array of questions for the game
-extern void initialize_game(void);
+void initialize_game(void)
+{
+    // initialize each question struct and assign it to the questions array
+  
+}
 
 // Displays each of the remaining categories and question dollar values that have not been answered
-extern void display_categories(void);
+void display_categories(void)
+{
+    // print categories and dollar values for each unanswered question in questions array
+    printf("On the menu tonight:\n");
+    for (int i = 0; i < NUM_CATEGORIES; i++)
+    {
+        if (!quest[0+i].answered || !quest[1+i].answered || !quest[2+i].answered || !quest[3+i].answered)
+        {
+            printf ("%-5s %-5s\t", categories[i], " ");
+ 
+                for (int j = 0; j < 4; j++)
+                {
+                    if (quest[i*4 + j].answered == false)
+                    {
+                        printf("%-2d \t", quest[i*4 +j].value);
+                    }
+			        else printf("%-2s\t", " ");
+                }
+        }
+        printf("\n"); //this spacing is necessary, it was getting on my nerves
+    }
+    
+    printf("Pick your poison: ");
+    
+}
 
 // Displays the question for the category and dollar value
-extern void display_question(char *category, int value);
+void display_question(char *category, int value)
+{
+   //printf("%s  %d\n", category, value);
+    for (int i=0; i<NUM_QUESTIONS; i++)
+    {
+        if ( ( strcmp(quest[i].category, category) == 0 ) && (quest[i].value == value) )
+        {
+        printf("\033[1;34m\t\t%s\033[0m\n", quest[i].question);
+        quest[i].answered = true; //we might as well mark it here
+        break; //this might fix something
+        }
+    }
+}
 
 // Returns true if the answer is correct for the question for that category and dollar value
-extern bool valid_answer(char *category, int value, char *answer);
+bool valid_answer(char *category, int value, char *answer)
+{
+    int test = 0;
+    if (answer[strlen(answer)-1] == '\n') { answer[strlen(answer)-1] = '\0'; } //remove newline     
+    for (int i=0; i<strlen(answer); i++)
+    {
+        if (answer[i] == ' ')
+        {
+            test++;
+        }
+    }
+
+    if (! (test == 2) ) return false;
+  	char *p = strtok(answer, " ");
+    if ( !(strcmp(&p, "WHO") || strcmp(&p, "WHAT") ) ) return false; 
+    if (p != NULL)  p = strtok(NULL, " ");
+    if ( !strcmp(&p, "IS") ) return false;
+    if (p != NULL)  p = strtok(NULL, " ");
+    strcpy(answer, p);
+
+
+    for (int i=0; i<NUM_QUESTIONS; i++)
+    {
+        if ( ( strcmp(quest[i].category, category) == 0 ) && (quest[i].value == value) )
+        {
+            if ( strcmp(quest[i].answer, answer) == 0 ) 
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
 
 // Returns true if the question has already been answered
-extern bool already_answered(char *category, int value);
+bool already_answered(char *category, int value)
+{
+    for (int i=0; i<NUM_QUESTIONS; i++)
+    {
+        if ( ( strcmp(quest[i].category, category) == 0 ) && (quest[i].value == value) )
+        {
+            if ( quest[i].answered == false ) 
+            {
+                return false;
+            }
+            else { return true; }
+        }
+    }
 
-#endif /* QUESTIONS_H_ */
+}
